7e35cd49a954c7d95e42ce954a90ce2950b61d57
kagawagao <kingsongao1221@gmail.com>
2020-10-12 10:04:28 +0800

    chore: update babel config

--- packages/babel-preset-lib/lib/index.js
+++ packages/babel-preset-lib/lib/index.js
@@ -15,42 +15,72 @@ module.exports = declare(
           '@babel/preset-env',
           {
             modules: false,
+            useBuiltIns: 'usage',
+            corejs: 3,
             targets: {
-              browsers: 'last 2 versions, IE >= 11',
+              chrome: 58,
             },
           },
         ],
         '@babel/preset-typescript',
       ],
-      plugins: ['@babel/plugin-proposal-class-properties'],
+      plugins: [
+        '@babel/plugin-proposal-class-properties',
+        [
+          '@babel/plugin-transform-runtime',
+          {
+            version: require('@babel/runtime/package.json').version,
+          },
+        ],
+      ],
       env: {
         commonjs: {
           presets: [
             [
               '@babel/preset-env',
               {
+                useBuiltIns: 'usage',
+                corejs: 3,
                 targets: {
-                  browsers: 'last 2 versions, IE >= 11',
+                  chrome: 58,
                 },
               },
             ],
             '@babel/preset-typescript',
           ],
-          plugins: ['@babel/plugin-proposal-class-properties'],
+          plugins: [
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                version: require('@babel/runtime/package.json').version,
+              },
+            ],
+            '@babel/plugin-proposal-class-properties',
+          ],
         },
         test: {
           presets: [
             [
               '@babel/preset-env',
               {
+                useBuiltIns: 'usage',
+                corejs: 3,
                 targets: {
-                  browsers: 'last 2 versions, IE >= 11',
+                  chrome: 58,
                 },
               },
             ],
             '@babel/preset-typescript',
           ],
-          plugins: ['@babel/plugin-proposal-class-properties'],
+          plugins: [
+            [
+              '@babel/plugin-transform-runtime',
+              {
+                version: require('@babel/runtime/package.json').version,
+              },
+            ],
+            '@babel/plugin-proposal-class-properties',
+          ],
         },
       },
     }
