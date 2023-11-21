2eadbd075db095d2c492a114b671b56ca8957d77
Jingsong Gao <kingsongao1221@gmail.com>
2023-11-21 15:03:38 +0800

    feat: remove cli and rewrite with typescript (#347)

---
+++ packages/prettier-config-pangu/src/index.ts
@@ -0,0 +1,26 @@
+import type { Config } from 'prettier'
+
+const config: Config = {
+  printWidth: 120,
+  tabWidth: 2,
+  useTabs: false,
+  semi: false,
+  singleQuote: true,
+  quoteProps: 'as-needed',
+  jsxSingleQuote: false,
+  trailingComma: 'all',
+  bracketSpacing: true,
+  // jsxBracketSameLine: false, // [Deprecated v2.4.0](https://prettier.io/docs/en/options.html#deprecated-jsx-brackets)
+  bracketSameLine: false,
+  arrowParens: 'always',
+  requirePragma: false,
+  insertPragma: false,
+  proseWrap: 'preserve',
+  htmlWhitespaceSensitivity: 'css',
+  vueIndentScriptAndStyle: false,
+  endOfLine: 'lf',
+  embeddedLanguageFormatting: 'auto',
+}
+
+export default config
+module.exports = config
